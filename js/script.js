1322774b8f1db70a75c59f9a7f80f3329903d55a
Filipe Bacof <filipebacof@gmail.com>
2022-10-13 09:22:33 -0300

    gerador de senhas finalizado

--- js/script.js
+++ js/script.js
@@ -22,7 +22,27 @@ const getSymbol = () => {
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = "";
+    const passwordLength = 10;
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    ]
+    for (let i = 0; i < passwordLength; i=i+4) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+            password+= randomValue;
+        });
+    }
+    password=password.slice(0, passwordLength);
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password;
+};
+
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
-
+    generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
 });
