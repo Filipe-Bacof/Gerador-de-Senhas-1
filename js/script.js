14d39ec14ce3aaaca4e4668e8e31f5e5df43afff
Filipe Bacof <filipebacof@gmail.com>
2022-10-13 08:48:03 -0300

    funções que geram caracteres feito

--- js/script.js
+++ js/script.js
@@ -6,10 +6,22 @@ const generatedPasswordElement = document.querySelector("#generated-password");
 
 // Funções
 const getLetterLowerCase = () => {
-    console.log(String.fromCharCode(10));
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-getLetterLowerCase();
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!?@#$&*+-";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
 
