08006446cf37b1c53178db22a8ed47325a8eae12
Filipe Bacof <filipebacof@gmail.com>
2022-10-13 08:37:53 -0300

    javascript inicializado

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Seleção de Elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+
+
+// Funções
+const getLetterLowerCase = () => {
+    console.log(String.fromCharCode(10));
+}
+
+getLetterLowerCase();
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+
+});
